9f6640168f37f7b4594b2d9616a8eb052f25279a
rrecord <137208283+rrecord@users.noreply.github.com>
2024-09-06 02:12:02 +0400

    sdf

--- app/ui/footer.tsx
+++ app/ui/footer.tsx
@@ -8,7 +8,7 @@ const Footer = () => {
   const pathname = usePathname();
 
   return (
-    <footer className="fixed bottom-0 h-[3.5rem] dark:bg-[#0c0b0b] transition-all bg-[#f2f2f2] w-full grid grid-cols-4 p-1.5">
+    <footer className="absolute 1xl:fixed bottom-0 h-[3.5rem] dark:bg-[#0c0b0b] transition-all bg-[#f2f2f2] w-full grid grid-cols-4 p-1.5">
       {footerLinks.map((e, i) => (
         <Link key={i} href={e.href} className={`flex items-center justify-center rounded-lg ${pathname === e.href ? 'dark:bg-[#100f0f] bg-[#e0e0e0]' : ''} transition-all`}>
           <span className="dark:text-white text-black transition-all">
